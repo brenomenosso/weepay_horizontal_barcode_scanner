28d7a34b5a8c9ec181f036917b84dea7de5d77a5
Breno Menosso RIgon <brenomenosso200@gmail.com>
2023-10-20 11:43:43 -0300

    .

--- lib/horizontal_barcode_scanner.dart
+++ lib/horizontal_barcode_scanner.dart
@@ -23,7 +23,7 @@ class HorizontalBarcodeScanner {
     bool showLine = true,
     Color lineColor = Colors.blue,
     double lineWidth = 2,
-    double scannerHeight = 170,
+    double scannerHeight = 160,
     Widget loadingWidget = const Text(
       'Loading...',
       style: TextStyle(
